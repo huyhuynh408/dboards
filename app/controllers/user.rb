12eac149e6fe8b2c3df901b166cb8ac663e3724a
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-02 15:44:36 -0500

    various

---
+++ app/controllers/user.rb
@@ -0,0 +1,27 @@
+get '/signup' do
+  erb :'user/signup'
+end
+
+post '/signup' do
+  user = User.new(params[:user])
+  password = params[:password]
+
+  if user.valid?
+    user.save
+    redirect("/user")
+  else
+    redirect("/signup")
+  end
+end
+
+get '/user' do
+  "Hello World"
+end
+
+get '/login' do
+  "Hello World"
+end
+
+get '/logout' do
+  "Hello World"
+end
